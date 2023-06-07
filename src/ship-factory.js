892c4b0fa95db32027aeae6fe63cc535d3f4c522
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-07 15:00:31 +0200

    Create hit and isSunk functions in ship object

--- src/ship-factory.js
+++ src/ship-factory.js
@@ -2,10 +2,19 @@ function Ship(shipLength) {
   const ship = {
     shipLength,
     hits: 0,
-    isSunk: false,
+    sunk: false,
+    hit() {
+      this.hits += 1;
+      this.isSunk();
+    },
+    isSunk() {
+      if (this.hits === this.shipLength) {
+        this.sunk = true;
+      }
+    }
   }
 
-  return { ship }
+  return ship
 }
 
 export default Ship;
