ae7d36c66ba611c685dc82ab2f6189da657c46c8
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-08 07:03:09 +0200

    Update Jest dependency options

--- jest.config.js
+++ jest.config.js
@@ -33,17 +33,3 @@ module.exports = {
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
 };
-
-module.exports = {
-  modulePaths: ['/shared/vendor/modules'],
-  moduleFileExtensions: ['js', 'jsx'],
-  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
-
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-
-    '^react(.*)$': '<rootDir>/vendor/react-master$1',
-    '^config$': '<rootDir>/configs/app-config.js',
-  },
-};
