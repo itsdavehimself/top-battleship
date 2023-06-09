0626d2b28f903521065d10e1b7af24831aa7ae45
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-09 15:11:12 +0200

    Create gameboard module

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,13 +1,17 @@
+import Ship from "./ship-factory";
+import Cell from "./cell";
+
 function Gameboard() {
   const rows = 10;
   const columns = 10;
   const board = [];
+  const placedShips = [];
 
   const makeBoard = () => {
     for (let i = 0; i < rows; i += 1) {
       board[i] = [];
       for (let j = 0; j < columns; j += 1) {
-        board[i].push(0);
+        board[i].push(Cell());
       }
     }
   }
@@ -16,7 +20,38 @@ function Gameboard() {
 
   const getBoard = () => board;
 
-  return { getBoard, board }
+  const placeShip = (startPos, direction, length, shipType) => {
+    let validMove = true;
+    if (direction === 'vertical') {
+      for (let i = 0; i < length; i += 1) {
+        if (board[startPos[0] + i][startPos[1]].getValue() !== 0) {
+          validMove = false;
+          return;
+          }
+        }
+      for (let i = 0; i < length; i += 1) {
+        if (validMove) {
+          board[startPos[0] + i][startPos[1]].markCell(shipType);
+        }
+      }
+      } else {
+      for (let i = 0; i < length; i += 1) {
+        if (board[startPos[0]][startPos[1] + i].getValue() !== 0) {
+          validMove = false;
+          return;
+          }
+        }
+      for (let i = 0; i < length; i += 1) {
+        if (validMove) {
+          board[startPos[0]][startPos[1] + i].markCell(shipType);
+        }
+      }
+      }
+      const newShip = Ship(length, shipType);
+      placedShips.push(newShip);
+    }
+
+  return { getBoard, placeShip }
 }
 
 export default Gameboard;
