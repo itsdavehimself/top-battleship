9796d7aea06c6850b65192b003b024ca7024f5d4
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-15 12:27:49 +0200

    Create function to render gameboard on screen

---
+++ src/DOM-gameboards.js
@@ -0,0 +1,37 @@
+const userDiv = document.querySelector('.user-board');
+const CPUDiv = document.querySelector('.cpu-board');
+
+function renderBoards(userBoard, cpuBoard) {
+  userBoard.getBoard().forEach((row, rowIndex) => {
+    row.forEach((cell, cellIndex) => {
+      const cellButton = document.createElement('button');
+      const cellValue = cell.getValue();
+      cellButton.classList.add('user-board-button');
+      cellButton.dataset.cellRow = `${rowIndex}`;
+      cellButton.dataset.cellColumn = `${cellIndex}`;
+      cellButton.dataset.cellValue = `${cellValue}`;
+
+      if (cell.getValue() > 0) {
+        cellButton.classList.add('user-placed-ship');
+      }
+      userDiv.appendChild(cellButton)
+    })
+  })
+
+  cpuBoard.getBoard().forEach((row, rowIndex) => {
+    row.forEach((cell, cellIndex) => {
+      const cellButton = document.createElement('button');
+      const cellValue = cell.getValue();
+      cellButton.classList.add('cpu-board-button');
+      cellButton.dataset.cellRow = `${rowIndex}`;
+      cellButton.dataset.cellColumn = `${cellIndex}`;
+      cellButton.dataset.cellValue = `${cellValue}`;
+      if (cell.getValue() > 0) {
+        cellButton.classList.add('cpu-placed-ship');
+      }
+      CPUDiv.appendChild(cellButton);
+    })
+  })
+};
+
+export default renderBoards
