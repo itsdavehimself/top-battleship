b1da9df8d5e588ab260a9dc8a71c973cad121817
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-06-15 12:42:45 +0200

    Create function to place cpu ships

--- src/game-controller.js
+++ src/game-controller.js
@@ -16,6 +16,69 @@ function Game() {
     renderBoards(userBoard, cpuBoard);
   }
 
+  const cpuShipPlacer = () => {
+    const randomCoordinates = () => {
+      const x = Math.floor(Math.random() * 9);
+      const y = Math.floor(Math.random() * 9);
+      return [x, y]
+    }
+    
+    const directionGenerator = () => {
+      const direction = Math.floor(Math.random() * 2);
+      if (direction === 0) {
+        return 'horizontal'
+      }
+      return 'vertical'
+    }
+
+    const placeCPUCarrier = () => {
+      const carrier = cpuBoard.placeShip(randomCoordinates(), directionGenerator(), 5, 1);
+      if (carrier === null) {
+        placeCPUCarrier();
+      }
+      return carrier;
+    }
+
+    const placeCPUBattleship = () => {
+      const battleship = cpuBoard.placeShip(randomCoordinates(), directionGenerator(), 4, 2);
+      if (battleship === null) {
+        placeCPUBattleship();
+      }
+      return battleship;
+    }
+
+    const placeCPUDestroyer = () => {
+      const destroyer = cpuBoard.placeShip(randomCoordinates(), directionGenerator(), 3, 3);
+      if (destroyer === null) {
+        placeCPUDestroyer();
+      }
+      return destroyer;
+    }
+
+    const placeCPUSubmarine = () => {
+      const submarine = cpuBoard.placeShip(randomCoordinates(), directionGenerator(), 3, 4);
+      if (submarine === null) {
+        placeCPUSubmarine();
+      }
+      return submarine;
+    }
+
+    const placeCPUPatrolBoat = () => {
+      const patrolBoat = cpuBoard.placeShip(randomCoordinates(), directionGenerator(), 2, 5);
+      if (patrolBoat === null) {
+        placeCPUPatrolBoat();
+      }
+      return patrolBoat;
+    }
+
+    placeCPUCarrier();
+    placeCPUBattleship();
+    placeCPUDestroyer();
+    placeCPUSubmarine();
+    placeCPUPatrolBoat();
+    updateBoard();
+  }
+ 
 }
 
 export default Game;
